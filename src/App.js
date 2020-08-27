8339171061d35bfc025c5f1c5b5b6b8ffe4b5275
Luis Felipe <luisfsosa@gmail.com>
2020-08-27 16:51:32 -0500

    change display Name

--- src/App.js
+++ src/App.js
@@ -4,19 +4,30 @@ import './App.css';
 
 function App() {
 
-    const [response, setResponse] = useState('');
+    const [agentName, setAgentName] = useState('');
+    const [messsage, setMesssage] = useState('');
     const [backGroundColor, setBackGroundColor] = useState('App-header');
 
     useEffect(() => {
         const socket =  new WebSocket('wss://lp0vui8n6f.execute-api.us-east-1.amazonaws.com/test');
         socket.onmessage = evt => {
             // add the new message to state
+
+            let messageObject = JSON.parse(evt.data);
+
+            console.log(messageObject);
+            console.log(messageObject.agentName);
+
             setBackGroundColor('App-header2');
-            setResponse(evt.data);
+            setAgentName(messageObject.agentName);
+            setMesssage(messageObject.messsage);
+
+
 
             setTimeout(() => {
                 setBackGroundColor('App-header');
-                setResponse("");
+                setAgentName('');
+                setMesssage('');
             }, 5000);
         };
 
@@ -28,7 +39,8 @@ function App() {
             <header className={backGroundColor}>
                 <img src={logo} className="App-logo" alt="logo" />
                 <h1>Hello from LIVEVOX</h1>
-                <h1>{response}</h1>
+                <h1>{agentName}</h1>
+                <h3>{messsage}</h3>
             </header>
         </div>
     );
