650d184a4fa18179ad5f6babfe50e924a768a10e
Luis Felipe <luisfsosa@gmail.com>
2020-08-27 15:59:18 -0500

    add websocket

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
+
+    const [response, setResponse] = useState('');
+    const [backGroundColor, setBackGroundColor] = useState('App-header');
+
+    useEffect(() => {
+        const socket =  new WebSocket('wss://lp0vui8n6f.execute-api.us-east-1.amazonaws.com/test');
+        socket.onmessage = evt => {
+            // add the new message to state
+            setBackGroundColor('App-header2');
+            setResponse(evt.data);
+
+            setTimeout(() => {
+                setBackGroundColor('App-header');
+                setResponse("");
+            }, 5000);
+        };
+
+
+    }, []);
+
     return (
         <div className="App">
-            <header>
+            <header className={backGroundColor}>
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1>We now have Auth!</h1>
+                <h1>Hello from LIVEVOX</h1>
+                <h1>{response}</h1>
             </header>
-            <AmplifySignOut />
         </div>
     );
 }
 
-export default withAuthenticator(App);
+export default App;
